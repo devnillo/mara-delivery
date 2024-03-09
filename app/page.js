3e4e344e17b031601683cbcda05cbb96447e5593
danilo Lopes <daniloalveslopesdev2023@gmail.com>
2024-03-09 06:34:28 -0300

    criação da primeira seção do site

--- app/page.js
+++ app/page.js
@@ -5,7 +5,39 @@ export default function Home() {
   return (
     <>
       <HeaderComponent />
-      
+      <main className="mt-20 overflow-x-hidden">
+        <section className="bg-primary flex min-h-[75vh]">
+          <div className="left w-[50vw] flex flex-col justify-center">
+            <h2 className="pl-[5%] text-white font-semibold text-[4vw]">
+              Você está com fome?
+            </h2>
+            <p className="pl-[5%] text-white text-[2vw]">
+              Aqui você acha os melhores lanches!
+            </p>
+            <div className="searchArea mx-auto w-[90%] bg-white p-8 rounded-xl flex justify-between items-center mt-4">
+              <input
+                className=" w-[68%] p-2 bg-slate-200 placeholder:text-gray-600 focus:outline-primary"
+                type="text"
+                placeholder="Cidade, Loja, Lanche"
+              />
+              <button className="w-[30%] p-2 bg-primary1 text-white font-semibold">
+                Buscar
+              </button>
+            </div>
+          </div>
+          <div className="right w-[50vw] flex justify-center items-center overflow-hidden">
+            <img
+              className=" w-[30vw] min-w-[18rem]"
+              src="food.png"
+              alt="prato com macarrão"
+            />
+          </div>
+        </section>
+        
+        
+      </main>
+
+      <ContactFlutuantComponent />
     </>
   );
 }
